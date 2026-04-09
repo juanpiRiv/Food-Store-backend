654d7df4ce3f8cf3440781c401dd130c83d6aa1a
juanpiRiv <juanpirivero015@gmail.com>
2026-04-09 09:00:15 -0300

    feat: products model

--- src/main/java/org/example/model/Producto.java
+++ src/main/java/org/example/model/Producto.java
@@ -1,4 +1,109 @@
 package org.example.model;
 
-public class Producto {
+import java.util.Objects;
+
+public class Producto extends Base {
+    private String nombre;
+    private Double precio;
+    private String descripcion;
+    private int stock;
+    private String imagen;
+    private boolean disponible;
+
+    public Producto() {
+        super();
+    }
+
+    public Producto(Long id, String nombre, Double precio, String descripcion, int stock, String imagen, boolean disponible) {
+        super(id);
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.stock = stock;
+        this.imagen = imagen;
+        this.disponible = disponible;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public boolean tieneStock(int cantidad) {
+        return stock >= cantidad;
+    }
+
+    public void descontarStock(int cantidad) {
+        if (cantidad > 0 && stock >= cantidad) {
+            stock -= cantidad;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                ", descripcion='" + descripcion + '\'' +
+                ", stock=" + stock +
+                ", imagen='" + imagen + '\'' +
+                ", disponible=" + disponible +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Producto producto)) return false;
+        return Objects.equals(nombre, producto.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre);
+    }
 }
